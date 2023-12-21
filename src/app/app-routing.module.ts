8d1e94ee98b3e8bbb211415b94fc78200e492e7a
JuanCamilo9915 <jcamiloduque0215@gmail.com>
2023-12-21 16:40:23 -0500

    Se crea el componente route-not-found de tipo standalone en la carpeta de util, porque será el componente a invocar si una ruta no existe o no coincide.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./module/login/login.module')
+    .then(m => m.LoginModule)
+  },
+  {
+    path: 'features',
+    loadChildren: () => import('./module/features/features.module')
+    .then(m => m.FeaturesModule)
+  },
+  { path: '', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/utils/route-not-found/route-not-found.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-route-not-found',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './route-not-found.component.html',
+  styleUrls: ['./route-not-found.component.css']
+})
+export class RouteNotFoundComponent {
+
+}
